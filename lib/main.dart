84f172bb72e4b85f163d1d307e09b933a2570be8
gosteve <nomuojamaro@gmail.com>
2020-04-27 08:12:00 +0100

    Refactored codebase from a stateless widget into a stateful widget so as to be able to track the dice number on the dicepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNum.png'),
+              ),
+              onPressed: () {
+                print('left button pressed');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'images/dice$rightDiceNum.png',
+              ),
+              onPressed: () {
+                print('Right button pressed');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
